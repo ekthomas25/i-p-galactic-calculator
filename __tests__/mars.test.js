9ac0c1623bbba563d68859e3373ed0401e0a51dd
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-11 15:03:16 -0800

    Fix typo

--- __tests__/mars.test.js
+++ __tests__/mars.test.js
@@ -2,7 +2,7 @@ import { mars } from './../src/js/mars'
 
 describe('mars', () => {
 
-  test('should return the inputted age divided by .64 rounded to the nearest two decimal', () => {
-    expect(mars(20)).toEqual(10.64;
+  test('should return the inputted age divided by 1.88 rounded to the nearest two decimal', () => {
+    expect(mars(20)).toEqual(10.64);
   });
 });
