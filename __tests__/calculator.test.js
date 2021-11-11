3aabd4526115137f50b7ff8514b7771781cc09e4
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-11 13:04:17 -0800

    create calculator() test 2.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -5,4 +5,8 @@ describe('calculator', () => {
   test('should iterate through an array and return a new array', () => {
     expect(calculator()).toEqual([.24, .62, 1.88, 11.86]);
   });
+  
+  test('should divide the argument by each array element and return a new array with integers rounded to the second decimal point', () => {
+    expect(calculator(20)).toEqual([83.33, 32.26, 10.64, 1.69]);
+  });
 });
