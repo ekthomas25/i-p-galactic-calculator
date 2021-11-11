a37c24466733ba478d2dfb6ecafd66b016be323e
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-11 12:57:58 -0800

    calculator() test 1 passing: returns new array

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,3 +1,8 @@
 export function calculator(age) {
- 
+  let planetArray = [.24, .62, 1.88, 11.86]
+  let ageArray = []
+  planetArray.forEach((nums) => {
+    ageArray.push(nums);
+});
+  return ageArray;
 }
