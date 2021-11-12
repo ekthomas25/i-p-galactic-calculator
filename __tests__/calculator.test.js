3e66ae616238cbf02dfc68fc88549cd3e7c51691
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-11 17:55:14 -0800

    create test 1

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,12 +1,16 @@
-import { calculator } from './../src/js/calculator'
+import { Calculator, calculator } from './../src/js/calculator'
 
 describe('calculator', () => {
+  
+  let newPlanet;
+  
+  beforeEach(() => {
+    newPlanet = new Calculator(20, 50)
+  });
 
-  test('should iterate through an array and return a new array', () => {
-    expect(calculator(1)).toEqual([4.17, 1.61, .53, .08]);
+  test('should return an object with the values of age, expectancy', () => {
+    expect(newPlanet.age).toEqual(20);
+    expect(newPlanet.expectancy).toEqual(50);
   });
   
-  test('should divide the argument by each array element and return a new array with integers rounded to the second decimal point', () => {
-    expect(calculator(20)).toEqual([83.33, 32.26, 10.64, 1.69]);
-  });
 });
