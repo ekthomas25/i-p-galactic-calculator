0a8ade118ab404da9ccc51afc6b281c629f0241a
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-11 18:27:54 -0800

    Test 2 fail

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,5 +1,13 @@
 export class Calculator {
   constructor(age, expectancy, planetAge, planetExpect, yearsLeft) {
-  
+    this.age = age;
+    this.expectancy = expectancy;
+    this.planetAge = planetAge;
+    this.planetExpect = planetExpect;
+    this.yearsLeft = yearsLeft;
+  }
+
+  mercury() {
+    
   }
 }
