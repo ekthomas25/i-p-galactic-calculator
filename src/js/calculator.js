a03bbaec7a9255f0d5d48552cd391b918a234396
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-11 17:58:39 -0800

    Test 1 good fail

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,9 +1,5 @@
-export function calculator(age) {
-  let planetArray = [.24, .62, 1.88, 11.86]
-  let ageArray =[]
-
-  planetArray.forEach((nums) => {
-    ageArray.push((Math.round((age/nums)*100)/100));
-});
-  return ageArray;
+export class Calculator {
+  constructor(age, expectancy, planetAge, planetExpect, yearsLeft) {
+  
+  }
 }
