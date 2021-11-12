95e2e3891b3bf43fbb394297e9ab6c2d99ae427b
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-11 18:26:30 -0800

    Add test 2

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -12,5 +12,13 @@ describe('calculator', () => {
     expect(newPlanet.age).toEqual(20);
     expect(newPlanet.expectancy).toEqual(50);
   });
+
+  test('should return an object with the values of age, expectancy, planetAge, and planetExpect for the planet mercury', () => {
+    newPlanet.mercury();
+    expect(newPlanet.age).toEqual(20);
+    expect(newPlanet.expectancy).toEqual(50);
+    expect(newPlanet.planetAge).toEqual(83.33);
+    expect(newPlanet.planetExpect).toEqual(208.33);
+  })
   
 });
