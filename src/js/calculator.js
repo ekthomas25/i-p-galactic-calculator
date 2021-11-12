67251f8034e3ba1123a3d6af4b061b17f5c9e774
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-11 18:59:07 -0800

    Test 3 good fail

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -11,4 +11,8 @@ export class Calculator {
     this.planetAge = Math.round((this.age/.24)*100)/100;
     this.planetExpect = Math.round((this.expectancy/.24)*100)/100;
   }
+
+  yearsRemain() {
+    
+  }
 }
